eecb4d46d05dae516ac9308b979a734c04ebe8cf
Emerson Lopes <emersonphilopes@gmail.com>
2023-05-03 16:45:24 -0300

    Base for MPI communication. Not working

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 
 #include <omp.h>
+#include <mpi/mpi.h>
 
 #include "utils.h"
 
@@ -15,37 +16,36 @@ bool placeQueen(int placedQueens[], int currCol, int insertRow, int size);
  * Params
  */
 int main(int argc, char *argv[]) {
-    int n, numThreads, numNodes;
-    double start, end;
-    solutionFound = false;
+    int n = argc > 1 ? atoi(argv[1]) : 8;
+    int t = argc > 3 ? atoi(argv[3]) : 8;
+    omp_set_num_threads(t);
 
-    getArgs(argc,  argv, &n, &numThreads, &numNodes);
+    int worldSize, worldRank;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
 
-    int finalSolution[n];
+    int *placedQueens = (int *) malloc(n * sizeof(int));
+    printf("My rank: %d;\tComm size: %d\n", worldRank, worldSize);
+//    #pragma omp parallel for shared(solutionFound, n, finalSolution) default(none)
+    if (worldRank == 0) {
+        for (int i = 0; i < n; i++) {
+            // each node should run the search for a given starting position
+            placedQueens[0] = i;
 
-    omp_set_num_threads(numThreads);
-
-    start = omp_get_wtime();
-
-    #pragma omp parallel for shared(solutionFound, n, finalSolution) default(none)
-    for (int i = 0; i < n; i++) {
-        // In parallel, try all solutions for the first column
-        int *placedQueens = (int *) malloc(n * sizeof(int));
-
-        placeQueen(placedQueens, 0, i, n);
-
-        if (isValidSolution(placedQueens, n)) {
-            memcpy(finalSolution, placedQueens, n * sizeof(int));
+            int dest = i % (worldSize - 1) + 1;
+            printf("dest: %d\n", dest);
+            MPI_Send(placedQueens, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
         }
-        free(placedQueens);
+    } else {
+        MPI_Recv(placedQueens, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        printf("Process %d received value %d from process 0\n", worldRank, placedQueens[0]);
     }
 
-    end = omp_get_wtime();
-    printf("Time taken with %d threads: %4.2f\n", numThreads, end - start);
+//    printSolution(finalSolution, n);
 
-    printSolution(finalSolution, n);
-//    for (int j = 0; j < n; j++)
-//        printf("Solution[%d] = %d\n", j, finalSolution[j]);
+    MPI_Finalize();
 
     return 0;
 }
